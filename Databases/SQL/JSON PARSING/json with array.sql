9989da03d3b3b424af33ddf795349a9dd55e8960
john <john.brumett@hsc.utah.edu>
2021-07-09 16:35:06 -0600

    updated notes

--- Databases/SQL/JSON PARSING/json with array.sql
+++ Databases/SQL/JSON PARSING/json with array.sql
@@ -33,3 +33,37 @@ WITH (
  sites VARCHAR(max) '$'
  );
 
+
+-----------------------OR without user root and just getting a field
+DECLARE @jsonMessage NVARCHAR(MAX);
+DECLARE @hi NVARCHAR(max);
+
+SET @jsonMessage = '{
+	
+		"userId": "fflintstone",
+		"userName": "Fred Flintstone",
+		"email": "fred@slaterockandgravel.com",
+		"groupingDataAdmin": true,
+		"userAdmin": false,
+		"allProviderManagementSites": false,
+        "active": true,
+		"providerManagementSites": [
+			"CHOP",
+			"UDCC",
+			"CINC"
+		]
+	
+}';
+
+SELECT @hi = userId
+FROM OPENJSON ( @jsonMessage, '$' )  
+WITH (   
+      userId varchar(MAX)  '$.userId' ,
+	userName varchar(MAX)  '$.userName',
+	email varchar(MAX)  '$.email',
+	groupingDataAdmin  varchar(MAX)  '$.groupingDataAdmin',
+	userAdmin VARCHAR(max) '$.userAdmin',
+	allProviderManagementSites VARCHAR(max) '$.allProviderManagementSites'
+ ); 
+
+ SELECT @hi
